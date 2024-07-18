d33751d2257f7f437effda64dda90e3691a75cd0
karangarg2002 <karangarg2002@gmail.com>
2024-07-18 11:32:16 +0530

    minor fix

--- client/src/components/shop/home/Slider.js
+++ client/src/components/shop/home/Slider.js
@@ -65,13 +65,13 @@ const Slider = (props) => {
               />
             </svg>
             <div className="absolute inset-0 flex items-center justify-center">
-              <a
+              {/* <a
                 href="#shop"
                 style={{ background: "#303031" }}
                 className="cursor-pointer box-border text-2xl text-white px-4 py-2 rounded"
               >
                 Shop Now
-              </a>
+              </a> */}
             </div>
           </>
         ) : null}
